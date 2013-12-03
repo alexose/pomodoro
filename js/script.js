c664daf1fcdaaf647ba763314ab45360756ad090
Alexander Ose <alex@alexose.com>
2013-12-03 11:37:37 -0800

    dummy data and table

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 (function(){
+    var data = {
+        time : '25:00',
+        tasks : [
+            { name : 'Task one' , completed : false },
+            { name : 'Task two' , completed : true },
+            { name : 'Task three' , completed : true },
+            { name : 'Task four' , completed : false },
+        ]
+    }
+
     var ractive = new Ractive({
         el : 'application',
-        template : '#pomodoro' 
+        template : '#pomodoro',
+        data : data
     })
 })()
