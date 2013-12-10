586503eb21a1137e2d773f2407acafb2feb661d0
Alexander Ose <alex@alexose.com>
2013-12-09 19:10:29 -0800

    localStorage support

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
 (function(){
     var pomodoro = 1000 * 60 * 25;
    
+    /*
+    tasks : [
+        { name : 'Task one' , remaining : 0, date : 1386641200000, active : false },
+        { name : 'Task two' , remaining : pomodoro, date : 1386643229000, active : false }, 
+        { name : 'Task three' , remaining : pomodoro, date : 1386641219000, active : false },
+        { name : 'Task four' , remaining : 0, date : 1386641221000, active: false },
+    ],
+    */
+
+    // Save and load methods for localStorage
+    var namespace = 'pomodoro-app';
+    function load(){
+        var result;
+
+        try {
+            if (localStorage && JSON){
+                var json = localStorage.getItem(namespace);
+                result = JSON.parse(json);
+            }
+        } catch(e){
+            console.log('Could not load data.  Starting from scratch.');
+        }
+        return result || [];
+    }
+    function save(){
+        try {
+            if (localStorage && JSON){
+                var array = this.data.tasks.slice(0);
+                localStorage.setItem(namespace, JSON.stringify(array));
+            }
+        } catch(e){
+            console.log('Could not save data.'); 
+        }
+    }
+
     var data = {
-        tasks : [
-            { name : 'Task one' , remaining : 0, date : 1386641200000, active : false },
-            { name : 'Task two' , remaining : pomodoro, date : 1386643229000, active : false }, 
-            { name : 'Task three' , remaining : pomodoro, date : 1386641219000, active : false },
-            { name : 'Task four' , remaining : 0, date : 1386641221000, active: false },
-        ],
+        tasks : load(),
         format : function(ts){
             var total = ts / 1000,
                 seconds = total % 60,
@@ -20,7 +50,16 @@
         },
     };
 
-    var ractive = new Ractive({
+    // TODO: trigger active
+
+    var app = Ractive.extend({
+        update : function(){
+            save.call(this);
+            this._super();
+        }
+    });
+
+    var ractive = new app({
         el : 'application',
         template : '#pomodoro',
         data : data
@@ -63,5 +102,10 @@
 
     ractive.on('clear', function(evt){
         evt.node.value = "";
+    });
+    
+    ractive.on('reset', function(evt){
+        this.data.tasks = [];
+        this.update();
     });
 })()
